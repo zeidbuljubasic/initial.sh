797f54d623ce73f262c718192c308b26bdb17668
Zeid Buljubasic <me@zeidb.com>
2022-07-27 19:43:37 +0200

    update

--- initial.sh
+++ initial.sh
@@ -1,120 +1,47 @@
 #!/bin/bash
-
 # Welcome message
 printf "$(clear; tput smacs)lqqqqqqqqqqqqqqqqqqqqqqqqqqqqk\nx $(tput rmacs)"
 printf "Initial Server Setup"
 printf "$(tput smacs)       x\nx $(tput rmacs)"
 printf "$(tput smso)me@zeidb.com$(tput rmso)"
 printf "$(tput smacs)               x\nmqqqqqqqqqqqqqqqqqqqqqqqqqqqqj\n\n$(tput rmacs)"
-
-
-# TASK 1: Updating
-printf "$(tput smso)Start the script? [y/n]:$(tput rmso) "
-read start_script
-
-if [ $start_script == "y" ]
-then
-	printf "Updating packages... "
-	apt update -y &> /dev/null
-	
-	if [ $? -eq 0 ]
-	then
-		printf "DONE\n"
-		
-		printf "Upgrading packages... "
-		apt upgrade -y &> /dev/null
-
-		if [ $? -eq 0 ]
-		then
-			printf "DONE\n\n"
-		else
-			printf "FAILED\n\nThere was a problem!\n\n"; exit
-		fi
-	else
-		printf "FAILED\n\nThere was a problem!\n\n"; exit
-	fi
-else
-	printf "\n\nBye!\n\n"; exit
-fi
-
-
-# Creating a sudo user
+printf "Starting the script (Press CTRL + C to terminate)...\n\n" && /usr/bin/sleep 5
+# Updating the system
+printf "Updating packages... "
+/usr/bin/apt update &> /dev/null && printf "DONE\n"
+printf "Upgrading packages... "
+/usr/bin/apt -y upgrade &> /dev/null && printf "DONE\n"
+# Creating sudo user
 printf "$(tput smso)Enter username for new account:$(tput rmso) "
 read username
-
-if [ -n "$username" ]
-then
-	adduser $username
-
-	if [ $? -eq 0 ]
-	then
-		usermod -aG sudo $username &> /dev/null
-
-		if [ $? -eq 0 ]
-		then
-			printf "\nCreating $username... DONE\nGranting $username... DONE\n"
-		else
-			printf "\nThere was a problem!\n\n"; exit
-		fi
-	else
-		printf "\nThere was a problem!\n\n"; exit
-	fi
-else
-	printf "\nInvalid username!\n\n"; exit
-fi
-
-
+/usr/sbin/adduser $username && usermod -aG sudo $username &> /dev/null
+printf "\nCreating and granting $username... " && /usr/bin/sleep 1 && printf "DONE\n"
 # Setting up the firewall
 printf "Enabling firewall... "
-ufw enable &> /dev/null || printf "FAIL\n\nThere was a problem!$(exit)"
-
-printf "$(sleep 1)DONE\nOpening port 22... "
-ufw allow 22 &> /dev/null || printf "FAIL\n\nThere was a problem!$(exit)"
-
-printf "$(sleep 1)DONE\n"
-
-
+/usr/sbin/ufw enable &> /dev/null && /usr/bin/sleep 1 && printf "DONE\n"
+printf "Opening port 22... "
+/usr/sbin/ufw allow 22 &> /dev/null && /usr/bin/sleep 1 && printf "DONE\n"
 # Setting up the timezone
 printf "Setting Central EU timezone... "
-timedatectl set-timezone Europe/Zurich &> /dev/null || printf "FAIL\n\nThere was a problem!$(exit)"
-printf "$(sleep 1)DONE\n"
-
-
+/usr/bin/timedatectl set-timezone Europe/Zurich &> /dev/null && /usr/bin/sleep 1 && printf "DONE\n"
 # Locking root account
 printf "Locking password-login for root... "
-passwd -l root &> /dev/null || printf "FAIL\n\nThere was a problem!$(exit)"
-printf "$(sleep 1)DONE\nDisabling SSH access with root... "
-
-if [ -e /etc/ssh/sshd_config ]
-then
-	sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config &> /dev/null || printf "FAIL\n\nThere was a problem!$(exit)"
-
-	printf "$(sleep 1)DONE\n"
-else
-	printf "NO sshd, CONTINUING\n"
+/usr/bin/passwd -l root &> /dev/null && /usr/bin/sleep 1 && printf "DONE\n"
+if [ -e /etc/ssh/sshd_config ]; then
+	printf "Disabling SSH access for root... "
+	/usr/bin/sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config &> /dev/null && /usr/bin/sleep 1 && printf "DONE\n"
 fi
-
-
-# Setting up hostname
+# Setting up the hostname
 printf "\n$(tput smso)Enter a new hostname:$(tput rmso) "
 read hostname
-
-if [ -n "$hostname" ]
+printf "Modifying /etc/hostname... "
+echo $hostname > /etc/hostname && printf "DONE\n"
+	
+printf "Modifying /etc/hosts... "
+	
+if grep -q "127.0.1.1" /etc/hosts
 then
-	printf "Modifying /etc/hostname... "
-	echo $hostname > /etc/hostname || printf "FAIL\n\nThere was a problem!$(exit)"
-	
-	printf "DONE\nModifying /etc/hosts... "
-	
-	if grep -q "127.0.1.1" /etc/hosts
-	then
-		sed -i "s/127.0.1.1.*/127.0.1.1 $hostname/" /etc/hosts || printf "FAIL\n\nThere was a problem!\n\n$(exit)"
-
-		printf "DONE\n"
-	else
-		sed -i "/127.0.0.1/a 127.0.1.1 $hostname" /etc/hosts || printf "FAIL\n\nThere was a problem!\n\n$(exit)"
-		printf "DONE\n"
-	fi
+	/usr/bin/sed -i "s/127.0.1.1.*/127.0.1.1 $hostname/" /etc/hosts && printf "DONE\n"
 else
-	printf "\n\nInvalid hostname!\n\n"; exit 
+	/usr/bin/sed -i "/127.0.0.1/a 127.0.1.1 $hostname" /etc/hosts && printf "DONE\n"
 fi
